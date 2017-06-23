81b566a93cbe6e3aea2ebcd49c70a87420ba2b7f
Kyle <kyle.c.r.fahey@gmail.com>
2017-06-23 14:19:28 -0500

    Integrate websockets into dashboard

--- app/components/dashboard.js
+++ app/components/dashboard.js
@@ -4,11 +4,39 @@ import styles from '../styles.css'
 import { Link } from 'react-router'
 
 
-const Dashboard = () => (
-    <div>
-        <h1>Dashboard</h1>
-     </div>
-);
+class Dashboard extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            tweet: '',
+            overallSentiment: 0
+        }
+    }
+    
+
+    componentWillMount() {
+        var connection = new WebSocket('ws://trump-sentiment.herokuapp.com/');
+        
+        connection.onopen = function () {
+            connection.send('Ping');
+        };
+        connection.onmessage = function (e) {
+            console.log(e)
+            if (e.data.main) {
+                this.state.tweet = e.data.main.featuredTweet
+                this.state.overallSentiment = e.data.main.sentiment
+            }
+        };
+    }
 
 
+    render() {
+        return (
+            <div>
+              <div className="Tweet">{this.state.tweet}</div>
+            </div>
+        );
+    }
+}
 export default Dashboard
