b3c1d8e67889fb5327ddbaf8361ae4d3c376106b
Kyle <kyle.c.r.fahey@gmail.com>
2017-07-04 08:29:16 -0500

    tweets working

--- app/components/dashboard.js
+++ app/components/dashboard.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from '../styles.css'
 
 import { Link } from 'react-router'
+import { Chart } from 'react-google-charts';
 
 
 class Dashboard extends React.Component {
@@ -10,7 +11,33 @@ class Dashboard extends React.Component {
         
         this.state = {
             tweet: '',
-            overallSentiment: 0
+            overallSentiment: 0,
+            lineData:[],
+            options: {
+                title: 'Senitment Trend',
+                vAxis: { minValue: -30, maxValue: 30 },
+                titleTextStyle: {
+                    color: '#d3d3d3',
+                    textPosition: 'none' 
+                },
+                backgroundColor: '#1a1a1a',
+                hAxis: {
+                    baselineColor: 'none',
+                    textStyle:{color: 'none'},
+                    gridlines: {
+                        color: 'transparent',
+                    }
+                },
+                vAxis: {
+                    textStyle: {color: '#d3d3d3'},
+                    gridlines: {
+                        count: 5,
+                        color: '#333333'
+                    }
+                },
+                legend: 'none'
+            },
+            
         }
     }
     
@@ -22,9 +49,13 @@ class Dashboard extends React.Component {
             e = JSON.parse(e.data)
             console.log(e)
             if (e.main) {
+                let lineArr = this.state.lineData.slice()
+                lineArr.push({"date": new Date().getTime(), score: e.main.sentiment.score })
                 this.setState({
                     tweet: e.main.featuredTweet,
-                    overallSentiment: e.main.sentiment
+                    overallSentiment: e.main.sentiment,
+                    lineData: lineArr
+                    
                 })
             }
         }
@@ -33,8 +64,21 @@ class Dashboard extends React.Component {
 
     render() {
         return (
-            <div>
-              <div className="Tweet">{this.state.tweet}</div>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-12 box">
+                        <div className="Tweet">
+                            {this.state.tweet}
+                            <p className="subtitle">Tweets</p> 
+                        </div>
+                    </div>
+                    <div className="col-md-2">
+                 
+                    </div>
+                    <div className="col-md-10">
+                    
+                    </div>
+                </div>
             </div>
         );
     }
